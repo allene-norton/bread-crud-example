f2b09f5ffea42aad6124b308168b2aabfddfed81
Allene Norton <norton.allene@gmail.com>
2021-12-08 16:58:08 -0600

    remove method-override

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
